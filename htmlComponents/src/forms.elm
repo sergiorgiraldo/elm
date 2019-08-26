76a17c02430ed5b2aa07fe2405ee56d2e9875c00
Sergio Giraldo <sergio@giraldo.com.br>
2019-08-26 16:51:28 -0300

    Updates by sgiraldo @ 20190826 16:51

--- htmlComponents/src/forms.elm
+++ htmlComponents/src/forms.elm
@@ -8,8 +8,10 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
-
-
+-- elm install fredcy/elm-parseint
+import ParseInt exposing (..) 
+-- elm install elm/regex
+import Regex exposing (..) 
 
 -- MAIN
 
@@ -21,9 +23,9 @@ main =
 
 -- MODEL
 
-
 type alias Model =
   { name : String
+  , age : String
   , password : String
   , passwordAgain : String
   }
@@ -31,15 +33,13 @@ type alias Model =
 
 init : Model
 init =
-  Model "" "" ""
-
-
+  Model "" "" "" ""
 
 -- UPDATE
 
-
 type Msg
   = Name String
+  | Age String
   | Password String
   | PasswordAgain String
 
@@ -50,35 +50,38 @@ update msg model =
     Name name ->
       { model | name = name }
 
+    Age age ->
+      { model | age = age }
+
     Password password ->
       { model | password = password }
 
     PasswordAgain password ->
       { model | passwordAgain = password }
 
-
-
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
   div []
     [ viewInput "text" "Name" model.name Name
+    , viewInput "numeric" "Age" model.age Age
     , viewInput "password" "Password" model.password Password
     , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
     , viewValidation model
     ]
 
-
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
   input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
-
 viewValidation : Model -> Html msg
 viewValidation model =
+  if 
   if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
+    if String.length model.password < 8 then
+      div [ style "color" "red" ] [ text "Passwords must be greater than 8!" ]
+    else
+      div [ style "color" "green" ] [ text "OK" ]
   else
     div [ style "color" "red" ] [ text "Passwords do not match!" ]
